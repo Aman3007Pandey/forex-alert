f5ba0687515d84b05590313fb5ee8bd0af35ec85
Aman Pandey <amanpandey3007@gmail.com>
2025-06-25 13:27:37 +0530

    added EUR?CHF

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ CURRENCY_PAIRS = {
     "GBP/USD": CurrencyPair("GBP/USD", 1.36000, 1.36313),
     "CAD/JPY": CurrencyPair("CAD/JPY", 105.626, 105.726),
     "EUR/GBP": CurrencyPair("EUR/GBP", 1.2500, 1.2600),
-    "EUR/CHF": CurrencyPair("EUR/CHF", 1.2500, 1.2600),
+    "EUR/CHF": CurrencyPair("EUR/CHF", 0.92800, 0.93200),
     "EUR/JPY": CurrencyPair("EUR/JPY", 1.2500, 1.2600),
     "GBP/JPY": CurrencyPair("GBP/JPY", 1.2500, 1.2600),
     "EUR/CAD": CurrencyPair("EUR/CAD", 1.2500, 1.2600),
